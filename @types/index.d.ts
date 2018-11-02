fb8485b658bc98948da628008a5547dfd3aaacff
Whapky <7638629+whapky@users.noreply.github.com>
2018-11-02 10:37:31 +0100

    fixing publish

--- @types/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { Bindings, FreeReducer } from './types';
-export declare function Store<T = any, P = T>(initialState: T, bound: Bindings<T, P>): (patch?: FreeReducer<T> | import("./types").IPatch<import("./types").IMap<import("./types").Reducer<T, P>>, P, string | number> | undefined) => import("rxjs/internal/Observable").Observable<T>;
