e66579d810646cb7065b4ea2c2648c7793cde2b9
tanfonto <ja@jas-MacBook-Pro.local>
2019-03-05 13:16:43 +0100

    conditional tests

--- test/utils/conditional.test.ts
+++ test/utils/conditional.test.ts
@@ -1,22 +1,36 @@
 import { test } from 'ava-ts';
-import { If } from '../../src/utils';
+import { If, when } from '../../src/utils';
 
 type List = Array<any>;
 
-const fn = If<object, string, List, number>(
+const weaveIf = If<object, string, List, number>(
   x => typeof x === 'object',
   () => [],
   () => 43
 );
 
-test('If should use left transformation when pred evaluates to true', t => {
-  const out: List = fn({});
+const weaveWhen = when<object, List>(x => typeof x === 'object', () => []);
 
-  t.deepEqual(out, []);
+test('If should use left transformation when pred evaluates to true', t => {
+  const actual: List = weaveIf({});
+
+  t.deepEqual(actual, []);
 });
 
 test('If should use right transformation when pred evaluates to false', t => {
-  const out: number = fn('');
+  const actual: number = weaveIf('');
 
-  t.deepEqual(out, 43);
+  t.deepEqual(actual, 43);
+});
+
+test('When should run transformation when pred evaluates to true', t => {
+  const actual: List = weaveWhen({});
+
+  t.deepEqual(actual, []);
+});
+
+test('When should default to identity when pred evaluates to false', t => {
+  const actual: number = weaveWhen(42);
+
+  t.deepEqual(actual, 42);
 });
